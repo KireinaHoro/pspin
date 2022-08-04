9e47e542cb31192f7aea4e19d771097d59aafdc9
Pengcheng Xu <xubin990510@gmail.com>
2022-08-04 11:19:02 +0200

    reduce size of L2 handler memory by half

--- hw/src/pspin_cfg_pkg.sv
+++ hw/src/pspin_cfg_pkg.sv
@@ -85,9 +85,9 @@ package automatic pspin_cfg_pkg;
   localparam int unsigned MEM_PKT_CUT_DW              = 512;
 
   // L2 handler memory
-  localparam int unsigned MEM_HND_SIZE                = 4*1024*1024;
+  localparam int unsigned MEM_HND_SIZE                = 2*1024*1024;
   localparam int unsigned MEM_HND_N_PAR_CUTS          = 32;
-  localparam int unsigned MEM_HND_CUT_N_WORDS         = 16384;
+  localparam int unsigned MEM_HND_CUT_N_WORDS         = 8192;
   localparam int unsigned MEM_HND_CUT_DW              = 64;
 
   // Interface types
