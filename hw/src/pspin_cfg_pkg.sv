b713ce1b7275ef98a479356b13e226ba4cd225dc
Pengcheng Xu <xubin990510@gmail.com>
2022-08-05 13:17:11 +0200

    reduce size of packet memory

--- hw/src/pspin_cfg_pkg.sv
+++ hw/src/pspin_cfg_pkg.sv
@@ -79,8 +79,8 @@ package automatic pspin_cfg_pkg;
   localparam int unsigned MEM_PROG_SIZE               = 32*1024;
 
   // L2 packet buffer
-  localparam int unsigned MEM_PKT_SIZE                = 4*1024*1024;
-  localparam int unsigned MEM_PKT_N_PAR_CUTS          = 32;
+  localparam int unsigned MEM_PKT_SIZE                = 2*1024*1024;
+  localparam int unsigned MEM_PKT_N_PAR_CUTS          = 16;
   localparam int unsigned MEM_PKT_CUT_N_WORDS         = 2048;
   localparam int unsigned MEM_PKT_CUT_DW              = 512;
 
