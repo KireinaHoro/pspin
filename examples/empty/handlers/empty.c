c6a40b7cf64c980382f58bf3cb625b650f8691ef
Pengcheng Xu <xubin990510@gmail.com>
2022-08-24 12:37:25 +0200

    update empty handler

--- examples/empty/handlers/empty.c
+++ examples/empty/handlers/empty.c
@@ -25,28 +25,28 @@
 #define NUM_INT_OP 0
 #endif
 
-volatile __attribute__((section(".l2_handler_data"))) uint8_t handler_mem[] = {0xde, 0xad, 0xbe, 0xef};
+volatile __attribute__((section(".l2_handler_data")))
+uint8_t handler_mem[] = {0xde, 0xad, 0xbe, 0xef};
 
-__handler__ void empty_ph(handler_args_t *args) 
-{
-    //printf("Payload handler!\n");
+__handler__ void empty_ph(handler_args_t *args) {
+  printf("Payload handler!\n");
 #if (NUM_INT_OP > 0)
-    volatile int xx = 0;
-    int x = xx;
-    for (int i=0; i<NUM_INT_OP; i++) {
-        x = x*i;
-    }
-    xx = x;
+  volatile int xx = 0;
+  int x = xx;
+  for (int i = 0; i < NUM_INT_OP; i++) {
+    x = x * i;
+  }
+  xx = x;
 #endif
 }
 
-void init_handlers(handler_fn * hh, handler_fn *ph, handler_fn *th, void **handler_mem_ptr)
-{
-    volatile handler_fn handlers[] = {NULL, empty_ph, NULL};
-    *hh = handlers[0];
-    *ph = handlers[1];
-    *th = handlers[2];
+void init_handlers(handler_fn *hh, handler_fn *ph, handler_fn *th,
+                   void **handler_mem_ptr) {
+  volatile handler_fn handlers[] = {NULL, empty_ph, NULL};
+  *hh = handlers[0];
+  *ph = handlers[1];
+  *th = handlers[2];
 
-    *handler_mem_ptr = (void*) handler_mem;
+  *handler_mem_ptr = (void *)handler_mem;
 }
 
