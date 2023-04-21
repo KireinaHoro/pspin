3b73bcb5e5721d15a2214f0eab93ad35d980e92b
Pengcheng Xu <xubin990510@gmail.com>
2023-04-21 22:50:08 +0200

    ping_pong on corundum

--- examples/ping_pong/handlers/ping_pong.c
+++ examples/ping_pong/handlers/ping_pong.c
@@ -20,46 +20,56 @@
 #define FROM_L1
 #endif
 
-__handler__ void pingpong_ph(handler_args_t *args)
-{
-    task_t* task = args->task;
-    ip_hdr_t *ip_hdr = (ip_hdr_t*) (task->pkt_mem);
+__handler__ void pingpong_ph(handler_args_t *args) {
+  printf("Packet @ %p (L2: %p) (size %d)\n", args->task->pkt_mem,
+         args->task->l2_pkt_mem, args->task->pkt_mem_size);
+
+  task_t *task = args->task;
+
+  eth_hdr_t *eth_hdr = (eth_hdr_t *)(task->pkt_mem);
+
+  ip_hdr_t *ip_hdr = (ip_hdr_t *)((uint8_t *)eth_hdr + sizeof(eth_hdr_t));
 #ifdef FROM_L2
-    uint8_t *nic_pld_addr = ((uint8_t*) (task->l2_pkt_mem)); 
+  uint8_t *nic_pld_addr = ((uint8_t *)(task->l2_pkt_mem));
 #else
-    uint8_t *nic_pld_addr = ((uint8_t*) (task->pkt_mem)); 
+  uint8_t *nic_pld_addr = ((uint8_t *)(task->pkt_mem));
 #endif
-    uint16_t pkt_pld_len = ip_hdr->length;
-    udp_hdr_t *udp_hdr = (udp_hdr_t*) (((uint8_t*) (task->pkt_mem)) + ip_hdr->ihl * 4);
 
-    uint32_t src_id = ip_hdr->source_id;
-    ip_hdr->source_id  = ip_hdr->dest_id;
-    ip_hdr->dest_id  = src_id;
+  udp_hdr_t *udp_hdr = (udp_hdr_t *)((uint8_t *)ip_hdr + ip_hdr->ihl * 4);
 
-    uint16_t src_port = udp_hdr->src_port;
-    udp_hdr->src_port = udp_hdr->dst_port;
-    udp_hdr->dst_port = src_port;
+  uint16_t pkt_pld_len = args->task->pkt_mem_size;
 
-    spin_cmd_t put;
-    spin_send_packet(nic_pld_addr, pkt_pld_len, &put);
+  mac_addr_t src_mac = eth_hdr->src;
+  eth_hdr->src = eth_hdr->dest;
+  eth_hdr->dest = src_mac;
 
-    //It's not strictly necessary to wait. The hw will enforce that the feedback is not
-    //sent until all commands issued by this handlers are completed.
+  uint32_t src_id = ip_hdr->source_id;
+  ip_hdr->source_id = ip_hdr->dest_id;
+  ip_hdr->dest_id = src_id;
+
+  uint16_t src_port = udp_hdr->src_port;
+  udp_hdr->src_port = udp_hdr->dst_port;
+  udp_hdr->dst_port = src_port;
+
+  spin_cmd_t put;
+  spin_send_packet(nic_pld_addr, pkt_pld_len, &put);
+
+  // It's not strictly necessary to wait. The hw will enforce that the feedback
+  // is not sent until all commands issued by this handlers are completed.
 #ifdef WAIT_POLL
-    bool completed = false;
-    do {
-        spin_cmd_test(put, &completed);
-    } while (!completed);
+  bool completed = false;
+  do {
+    spin_cmd_test(put, &completed);
+  } while (!completed);
 #elif defined(WAIT_SUSPEND)
-    spin_cmd_wait(put);
+  spin_cmd_wait(put);
 #endif
-
 }
 
-void init_handlers(handler_fn * hh, handler_fn *ph, handler_fn *th, void **handler_mem_ptr)
-{
-    volatile handler_fn handlers[] = {NULL, pingpong_ph, NULL};
-    *hh = handlers[0];
-    *ph = handlers[1];
-    *th = handlers[2];
+void init_handlers(handler_fn *hh, handler_fn *ph, handler_fn *th,
+                   void **handler_mem_ptr) {
+  volatile handler_fn handlers[] = {NULL, pingpong_ph, NULL};
+  *hh = handlers[0];
+  *ph = handlers[1];
+  *th = handlers[2];
 }
