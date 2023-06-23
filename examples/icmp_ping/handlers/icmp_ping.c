762d80dce090103914214a6959db47569dd1efdc
Pengcheng Xu <xubin990510@gmail.com>
2023-06-23 16:39:51 +0200

    icmp ping example with host

---
+++ examples/icmp_ping/handlers/icmp_ping.c
@@ -0,0 +1,211 @@
+// Copyright 2020 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pspin.h"
+#include <handler.h>
+#include <packets.h>
+#include <spin_dma.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if !defined(FROM_L2) && !defined(FROM_L1)
+#define FROM_L1
+#endif
+
+#define DO_HOST true
+
+#define DEBUG(...) printf(__VA_ARGS__)
+// #define DEBUG(...)
+
+#define NUM_HPUS_PER_CLUSTER 8
+#define NUM_CLUSTERS 2
+#define NUM_HPUS (NUM_HPUS_PER_CLUSTER * NUM_CLUSTERS)
+#define PAGE_SIZE 4096
+#define HPU_ID(args) (args->cluster_id * NUM_HPUS_PER_CLUSTER + args->hpu_id)
+#define HOST_ADDR(args)                                                        \
+  (((uint64_t)args->task->host_mem_high << 32) | args->task->host_mem_low)
+#define HOST_ADDR_HPU(args) (HOST_ADDR(args) + HPU_ID(args) * PAGE_SIZE)
+
+#define FLAG_DMA_ID(fl) ((fl)&0xf)
+#define FLAG_LEN(fl) (((fl) >> 8) & 0xff)
+#define FLAG_HPU_ID(fl) ((fl) >> 24 & 0xff)
+#define MKFLAG(id, len, hpuid)                                                 \
+  (((id)&0xf) | (((len)&0xff) << 8) | (((hpuid)&0xff) << 24))
+#define DMA_BUS_WIDTH 512
+#define DMA_ALIGN (DMA_BUS_WIDTH / 8)
+
+// TODO: refactor into common facility
+extern volatile uint64_t __host_flag[NUM_HPUS];
+static uint8_t dma_idx[NUM_HPUS];
+
+static volatile int32_t inflight_messages = 0;
+
+static inline uint16_t bswap_16(uint16_t v) {
+  return ((v & 0xff) << 8) | (v >> 8);
+}
+
+#define htons(x) bswap_16(x)
+#define ntohs htons
+
+// FIXME: only for ICMP echo
+typedef struct {
+  uint8_t type;
+  uint8_t code;
+  uint16_t checksum;
+  uint32_t rest_of_header;
+} icmp_hdr_t;
+
+typedef struct {
+  eth_hdr_t eth_hdr;
+  ip_hdr_t ip_hdr; // FIXME: assumes ihl=4
+  icmp_hdr_t icmp_hdr;
+} __attribute__((__packed__)) hdr_t;
+
+uint16_t ip_checksum(void *vdata, size_t length) {
+  // Cast the data pointer to one that can be indexed.
+  char *data = (char *)vdata;
+
+  // Initialise the accumulator.
+  uint32_t acc = 0xffff;
+
+  // Handle complete 16-bit blocks.
+  for (size_t i = 0; i + 1 < length; i += 2) {
+    uint16_t word;
+    memcpy(&word, data + i, 2);
+    acc += ntohs(word);
+    if (acc > 0xffff) {
+      acc -= 0xffff;
+    }
+  }
+
+  // Handle any partial block at the end of the data.
+  if (length & 1) {
+    uint16_t word = 0;
+    memcpy(&word, data + length - 1, 1);
+    acc += ntohs(word);
+    if (acc > 0xffff) {
+      acc -= 0xffff;
+    }
+  }
+
+  // Return the checksum in network byte order.
+  return htons(~acc);
+}
+
+__handler__ void pingpong_hh(handler_args_t *args) {
+  DEBUG(
+      "Start of message: @ %p (L2: %p) (size %d) flow_id %d inflight msgs %d\n",
+      args->task->pkt_mem, args->task->l2_pkt_mem, args->task->pkt_mem_size,
+      args->task->flow_id, inflight_messages);
+  amo_add(&inflight_messages, 1);
+}
+
+__handler__ void pingpong_th(handler_args_t *args) {
+  DEBUG("End of message: @ %p (L2: %p) (size %d) flow_id %d inflight msgs %d\n",
+        args->task->pkt_mem, args->task->l2_pkt_mem, args->task->pkt_mem_size,
+        args->task->flow_id, inflight_messages);
+  amo_add(&inflight_messages, -1);
+}
+
+__handler__ void pingpong_ph(handler_args_t *args) {
+  DEBUG("Packet @ %p (L2: %p) (size %d) flow_id %d inflight msgs %d\n",
+        args->task->pkt_mem, args->task->l2_pkt_mem, args->task->pkt_mem_size,
+        args->task->flow_id, inflight_messages);
+
+  task_t *task = args->task;
+
+  hdr_t *hdrs = (hdr_t *)(task->pkt_mem);
+  uint16_t pkt_len = args->task->pkt_mem_size;
+#ifdef FROM_L2
+  uint8_t *nic_pld_addr = ((uint8_t *)(task->l2_pkt_mem));
+#else
+  uint8_t *nic_pld_addr = ((uint8_t *)(task->pkt_mem));
+#endif
+
+  // ICMP ping: swap MAC and IP address
+  mac_addr_t src_mac = hdrs->eth_hdr.src;
+  hdrs->eth_hdr.src = hdrs->eth_hdr.dest;
+  hdrs->eth_hdr.dest = src_mac;
+
+  uint32_t src_id = hdrs->ip_hdr.source_id;
+  hdrs->ip_hdr.source_id = hdrs->ip_hdr.dest_id;
+  hdrs->ip_hdr.dest_id = src_id;
+
+  spin_cmd_t dma;
+  uint64_t flag_haddr = HOST_ADDR_HPU(args),
+           pld_haddr = HOST_ADDR_HPU(args) + DMA_ALIGN;
+
+  if (DO_HOST && HOST_ADDR(args) && args->task->host_mem_size >= NUM_HPUS * PAGE_SIZE) {
+    DEBUG("Host flag addr: %#llx\n", flag_haddr);
+
+    // DMA packet data
+    spin_dma_to_host(pld_haddr, (uint32_t)nic_pld_addr, pkt_len, 1, &dma);
+    spin_cmd_wait(dma);
+    DEBUG("Written packet data\n");
+
+    // FIXME: provide library function for comm to host
+    // prepare host notification
+    ++dma_idx[HPU_ID(args)];
+    volatile uint64_t flag_to_host =
+        MKFLAG(dma_idx[HPU_ID(args)], pkt_len, HPU_ID(args));
+
+    // write flag
+    spin_write_to_host(flag_haddr, flag_to_host, &dma);
+    spin_cmd_wait(dma);
+
+    // poll for host finish
+    uint64_t flag_from_host;
+    do {
+      flag_from_host = __host_flag[HPU_ID(args)];
+    } while (FLAG_DMA_ID(flag_to_host) != FLAG_DMA_ID(flag_from_host));
+    // FIXME: end
+
+    uint16_t host_pkt_len = FLAG_LEN(flag_from_host);
+
+    // DMA packet data back
+    spin_dma_from_host(pld_haddr, (uint32_t)nic_pld_addr, host_pkt_len, 1,
+                       &dma);
+    spin_cmd_wait(dma);
+
+    DEBUG("DMA roundtrip finished, packet from host size: %d\n", host_pkt_len);
+    if (FLAG_HPU_ID(flag_from_host) != HPU_ID(args)) {
+      printf("HPU ID mismatch in response flag!  Got: %lld\n",
+             FLAG_HPU_ID(flag_from_host));
+    }
+    pkt_len = host_pkt_len;
+  } else {
+    // calculate ICMP checksum ourselves
+    uint16_t ip_len = ntohs(hdrs->ip_hdr.length);
+    uint16_t icmp_len = ip_len - sizeof(ip_hdr_t);
+    hdrs->icmp_hdr.type = 0; // Echo-Reply
+    hdrs->icmp_hdr.checksum = 0;
+    hdrs->icmp_hdr.checksum =
+        ip_checksum((uint8_t *)&hdrs->icmp_hdr, icmp_len);
+    pkt_len = ip_len + sizeof(eth_hdr_t);
+  }
+
+  spin_cmd_t put;
+  spin_send_packet(nic_pld_addr, pkt_len, &put);
+  spin_cmd_wait(put);
+}
+
+void init_handlers(handler_fn *hh, handler_fn *ph, handler_fn *th,
+                   void **handler_mem_ptr) {
+  volatile handler_fn handlers[] = {pingpong_hh, pingpong_ph, pingpong_th};
+  *hh = handlers[0];
+  *ph = handlers[1];
+  *th = handlers[2];
+}
---
+++ examples/icmp_ping/host/icmp-ping.c
@@ -0,0 +1,144 @@
+#include "fpspin/fpspin.h"
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <immintrin.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define PSPIN_DEV "/dev/pspin0"
+
+volatile sig_atomic_t exit_flag = 0;
+static void sigint_handler(int signum) { exit_flag = 1; }
+
+// FIXME: only for ICMP echo
+typedef struct {
+  uint8_t type;
+  uint8_t code;
+  uint16_t checksum;
+  uint32_t rest_of_header;
+} icmp_hdr_t;
+
+typedef struct {
+  eth_hdr_t eth_hdr;
+  ip_hdr_t ip_hdr; // FIXME: assumes ihl=4
+  icmp_hdr_t icmp_hdr;
+} __attribute__((__packed__)) hdr_t;
+
+// http://www.microhowto.info/howto/calculate_an_internet_protocol_checksum_in_c.html
+uint16_t ip_checksum(void *vdata, size_t length) {
+  // Cast the data pointer to one that can be indexed.
+  char *data = (char *)vdata;
+
+  // Initialise the accumulator.
+  uint32_t acc = 0xffff;
+
+  // Handle complete 16-bit blocks.
+  for (size_t i = 0; i + 1 < length; i += 2) {
+    uint16_t word;
+    memcpy(&word, data + i, 2);
+    acc += ntohs(word);
+    if (acc > 0xffff) {
+      acc -= 0xffff;
+    }
+  }
+
+  // Handle any partial block at the end of the data.
+  if (length & 1) {
+    uint16_t word = 0;
+    memcpy(&word, data + length - 1, 1);
+    acc += ntohs(word);
+    if (acc > 0xffff) {
+      acc -= 0xffff;
+    }
+  }
+
+  // Return the checksum in network byte order.
+  return htons(~acc);
+}
+
+void ruleset_icmp_echo(fpspin_ruleset_t *rs) {
+  assert(NUM_RULES_PER_RULESET == 4);
+  *rs = (fpspin_ruleset_t){
+      .mode = FPSPIN_MODE_AND,
+      .r =
+          {
+              FPSPIN_RULE_IP,
+              FPSPIN_RULE_IP_PROTO(1), // ICMP
+              ((struct fpspin_rule){.idx = 8,
+                                    .mask = 0xff00,
+                                    .start = 0x0800,
+                                    .end = 0x0800}), // ICMP Echo-Request
+              FPSPIN_RULE_FALSE,                     // never EOM
+          },
+  };
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <ctx id>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  struct sigaction sa = {
+      .sa_handler = sigint_handler,
+      .sa_flags = 0,
+  };
+  sigemptyset(&sa.sa_mask);
+  if (sigaction(SIGINT, &sa, NULL)) {
+    perror("sigaction");
+    exit(EXIT_FAILURE);
+  }
+
+  int dest_ctx = atoi(argv[1]);
+  fpspin_ctx_t ctx;
+  fpspin_ruleset_t rs;
+  // custom ruleset
+  ruleset_icmp_echo(&rs);
+  if (!fpspin_init(&ctx, PSPIN_DEV, __IMG__, dest_ctx, &rs, 1)) {
+    fprintf(stderr, "failed to initialise fpspin\n");
+    goto fail;
+  }
+
+  // loading finished - application logic from here
+  while (true) {
+    if (exit_flag) {
+      printf("\nReceived SIGINT, exiting...\n");
+      break;
+    }
+    for (int i = 0; i < NUM_HPUS; ++i) {
+      uint64_t flag_to_host;
+      volatile uint8_t *pkt_addr;
+
+      if (!(pkt_addr = fpspin_pop_req(&ctx, i, &flag_to_host)))
+        continue;
+      volatile hdr_t *hdrs = (hdr_t *)pkt_addr;
+      uint16_t ip_len = ntohs(hdrs->ip_hdr.length);
+
+      // ICMP type and checksum
+      size_t icmp_len = ip_len - sizeof(ip_hdr_t);
+      hdrs->icmp_hdr.type = 0; // Echo-Reply
+      hdrs->icmp_hdr.checksum = 0;
+      hdrs->icmp_hdr.checksum =
+          ip_checksum((uint8_t *)&hdrs->icmp_hdr, icmp_len);
+
+      size_t return_size = ip_len + sizeof(eth_hdr_t);
+      uint64_t flag_from_host = MKFLAG(return_size);
+      fpspin_push_resp(&ctx, i, flag_from_host);
+    }
+  }
+
+  fpspin_exit(&ctx);
+  return EXIT_SUCCESS;
+
+fail:
+  fpspin_exit(&ctx);
+  return EXIT_FAILURE;
+}
