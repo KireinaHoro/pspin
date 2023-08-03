06b7c8c4887c4a76e7a5f9678d34bbb6f92ad741
Pengcheng Xu <xubin990510@gmail.com>
2023-08-03 10:59:11 +0200

    move ddt init to common host-side file

--- examples/datatypes/host/datatypes.c
+++ examples/datatypes/host/datatypes.c
@@ -1,5 +1,3 @@
-#include "fpspin/fpspin.h"
-
 #include <argp.h>
 #include <arpa/inet.h>
 #include <assert.h>
@@ -17,12 +15,6 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
-
-#include "../mpitypes/install/include/mpitypes_dataloop.h"
-#include "../typebuilder/ddt_io_read.h"
-
-#include "../handlers/include/datatype_descr.h"
-#include "../handlers/include/datatypes.h"
 
 #include "../include/datatypes_host.h"
 
@@ -161,8 +153,8 @@ typedef struct {
   int file_id;
   struct arguments args;
   uint32_t userbuf_size;
-  int num_received; // received but unclaimed (from parallel messages)
-  int num_elements; // number of datatype elements, from DDT bin
+  int num_received;      // received but unclaimed (from parallel messages)
+  uint32_t num_elements; // number of datatype elements, from DDT bin
 
   int dim; // dimension of dgemm matrices - current trial
   double *A, *B, *C;
@@ -220,49 +212,10 @@ static int setup_datatypes_spin(fpspin_ctx_t *ctx, int argc, char *argv[]) {
     return -1;
   }
 
-  FILE *f = fopen(args->type_descr_file, "rb");
-  if (!f) {
-    perror("open type descr");
-    exit(EXIT_FAILURE);
-  }
-  size_t datatype_mem_size = get_spin_datatype_size(f);
-  void *datatype_mem_ptr_raw = malloc(datatype_mem_size);
-  if (!datatype_mem_ptr_raw) {
-    fprintf(stderr, "failed to allocate datatype buffer\n");
-    return -1;
-  }
-
-  read_spin_datatype(datatype_mem_ptr_raw, datatype_mem_size, f);
-  fclose(f);
-
-  spin_datatype_t *dt_header = (spin_datatype_t *)datatype_mem_ptr_raw;
-
-  type_info_t dtinfo = dt_header->info;
-  uint32_t dtcount = dt_header->count;
-  uint32_t dtblocks = dt_header->blocks;
-
-  printf("Count: %u\n", dtcount);
-  printf("Blocks: %u\n", dtblocks);
-  printf("Size: %li\n", dtinfo.size);
-  printf("Extent: %li\n", dtinfo.extent);
-  printf("True extent: %li\n", dtinfo.true_extent);
-  printf("True LB: %li\n", dtinfo.true_lb);
-
-  app_data->num_elements = dtcount;
-  app_data->userbuf_size =
-      dtinfo.true_lb + MAX(dtinfo.extent, dtinfo.true_extent) * dtcount;
-  printf("Userbuf size: %d\n", app_data->userbuf_size);
-
   fpspin_ruleset_t rs;
   fpspin_ruleset_slmp(&rs);
   if (!fpspin_init(ctx, args->pspin_dev, __IMG__, args->dest_ctx, &rs, 1)) {
     fprintf(stderr, "failed to initialise fpspin\n");
-    return -1;
-  }
-
-  if (ctx->handler_mem.size < datatype_mem_size) {
-    fprintf(stderr, "handler mem too small to fit datatype: %d vs %ld\n",
-            ctx->handler_mem.size, datatype_mem_size);
     return -1;
   }
 
@@ -276,39 +229,16 @@ static int setup_datatypes_spin(fpspin_ctx_t *ctx, int argc, char *argv[]) {
   // clear host buffer
   memset(ctx->cpu_addr, 0, ctx->mmap_len);
 
-  // buffer area before copying onto the NIC
-  size_t nic_buffer_size = sizeof(spin_datatype_mem_t) + datatype_mem_size +
-                           sizeof(spin_core_state_t) * NUM_HPUS;
-  void *nic_buffer = malloc(nic_buffer_size);
-  if (!nic_buffer) {
-    fprintf(stderr, "failed to allocate nic buffer\n");
-    return -1;
-  }
-
   fpspin_addr_t nic_mem_base = ctx->handler_mem.addr;
-  // layout: spin_datatype_mem_t | ddt | spin_core_state_t
-  size_t nic_ddt_off = sizeof(spin_datatype_mem_t);
-
-  fpspin_addr_t nic_ddt_pos = nic_mem_base + nic_ddt_off;
-  uint8_t *nic_buffer_ddt_data = (uint8_t *)nic_buffer + nic_ddt_off;
-
-  // relocate datatype for NIC
-  memcpy(nic_buffer_ddt_data, datatype_mem_ptr_raw, datatype_mem_size);
-  remap_spin_datatype(nic_buffer_ddt_data, datatype_mem_size, nic_ddt_pos,
-                      true);
-
-  spin_datatype_mem_t *nic_buffer_ddt_descr = (spin_datatype_mem_t *)nic_buffer;
-  spin_datatype_t *nic_buffer_dt = (spin_datatype_t *)nic_buffer_ddt_data;
-
-  spin_core_state_t *nic_buffer_state =
-      (spin_core_state_t *)(nic_buffer_ddt_data + datatype_mem_size);
-  nic_buffer_ddt_descr->state =
-      (spin_core_state_t *)(nic_ddt_pos + datatype_mem_size);
-
-  for (int i = 0; i < NUM_HPUS; ++i) {
-    // segment replicated onto each core
-    nic_buffer_state[i].state = nic_buffer_dt->seg;
-    nic_buffer_state[i].params = nic_buffer_dt->params;
+  void *nic_buffer;
+  size_t nic_buffer_size = 0;
+  void *datatype_mem_ptr_raw = prepare_ddt_nicmem(
+      args->type_descr_file, ctx->handler_mem, &nic_buffer, &nic_buffer_size,
+      &app_data->num_elements, &app_data->userbuf_size);
+  printf("Userbuf size: %d\n", app_data->userbuf_size);
+  if (!nic_buffer_size) {
+    // ddt remap failed
+    return -1;
   }
 
   // copy to NIC memory
